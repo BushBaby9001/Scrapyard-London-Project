4d0293b22c07b6df478d83d77b79f8da2776fa4e
BushBaby9001 <bushbaby9001@gmail.com>
2025-03-15 15:04:39 +0000

    Add files via upload

--- Project.py
+++ Project.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -6,15 +7,16 @@ pygame.init()
 
 class Button:
 
-    def __init__(self, screen, colour, rect, border_radius=20):
+    def __init__(self, screen, colour, rect, border_radius=20, width=0):
         self.screen = screen
         self.colour = colour
         self.pos = rect
         self.rect = pygame.Rect(rect)
         self.border_radius = border_radius
+        self.width = width
 
     def draw(self):
-        pygame.draw.rect(self.screen, self.colour, self.pos, border_radius=self.border_radius)
+        pygame.draw.rect(self.screen, self.colour, self.pos, border_radius=self.border_radius, width=self.width)
 
     def interaction(self, mousepos):
         if self.rect.collidepoint(mousepos):
@@ -37,7 +39,7 @@ class Drawing:
 
     def draw(self):
         #pygame.draw.line(self.surface,self.col,(self.prevx,self.prevy),(self.x,self.y))
-        ink = pygame.draw.circle(self.surface,self.col,(self.x,self.y),self.w*1.2)
+        pygame.draw.circle(self.surface,self.col,(self.x,self.y),self.w*1.2)
 
         #ink = pygame.Rect(self.x, self.y, self.w, self.h)
         #pygame.draw.rect(self.surface, self.col, ink)
@@ -95,6 +97,12 @@ class Whiteboard:
         self.red = Button(self.screen, (255, 0, 0), (200, 50, 50, 50))
         self.green = Button(self.screen, (0, 255, 0), (300, 50, 50, 50))
         self.blue = Button(self.screen, (0, 0, 255), (400, 50, 50, 50))
+        self.yellow = Button(self.screen, (255, 255, 0), (500, 50, 50, 50))
+        self.purple = Button(self.screen, (128, 0, 255), (600, 50, 50, 50))
+        self.cyan = Button(self.screen, (0, 255, 255), (700, 50, 50, 50))
+        self.orange = Button(self.screen, (255, 128, 0), (800, 50, 50, 50))
+
+        self.widthbox = Button(self.screen, (0, 0, 0), (900, 50, 200, 50), width=3)
 
         self.mousepen = Drawing(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1], (0,0,0), 5,  5, self.screen)
 
@@ -111,6 +119,17 @@ class Whiteboard:
         self.red.draw()
         self.green.draw()
         self.blue.draw()
+        self.yellow.draw()
+        self.purple.draw()
+        self.cyan.draw()
+        self.orange.draw()
+
+        self.widthbox.draw()
+
+        font = pygame.font.SysFont("david", 32)
+        text_render = font.render("Random Width", True, (0, 0, 0))
+        pos = text_render.get_rect(center=self.widthbox.rect.center)
+        self.screen.blit(text_render, pos)
 
         run = True
         while run:
@@ -135,6 +154,7 @@ class Whiteboard:
                 self.mousepen.x = mousepos[0]
                 self.mousepen.y = mousepos[1]
 
+                # wall
                 if self.black.interaction(mousepos):
                     self.mousepen.col = self.black.interaction(mousepos)[1]
                 elif self.red.interaction(mousepos):
@@ -143,8 +163,19 @@ class Whiteboard:
                     self.mousepen.col = self.green.interaction(mousepos)[1]
                 elif self.blue.interaction(mousepos):
                     self.mousepen.col = self.blue.interaction(mousepos)[1]
+                elif self.yellow.interaction(mousepos):
+                    self.mousepen.col = self.yellow.interaction(mousepos)[1]
+                elif self.purple.interaction(mousepos):
+                    self.mousepen.col = self.purple.interaction(mousepos)[1]
+                elif self.cyan.interaction(mousepos):
+                    self.mousepen.col = self.cyan.interaction(mousepos)[1]
+                elif self.orange.interaction(mousepos):
+                    self.mousepen.col = self.orange.interaction(mousepos)[1]
 
-                if self.mousepen.y > 150:
+                elif self.widthbox.interaction(mousepos):
+                    self.mousepen.w = random.randint(1, 75)
+
+                if self.mousepen.y > 150 + self.mousepen.w*1.2:
                     self.mousepen.draw()
 
 
